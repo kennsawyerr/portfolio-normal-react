a03840447e0c82bc8cc5dae4d8e45d0e691e0795
Kehinde Sawyerr <kensawyerr@gmail.com>
2025-07-10 23:42:32 +0100

    working on marquee

--- src/components/PlayGround.jsx
+++ src/components/PlayGround.jsx
@@ -1,66 +1,61 @@
+import Marquee from "react-fast-marquee";
 const PlayGround = () => {
   return (
     <>
-      <div className="xui-container" style={{ overflow: "hidden" }}>
+      <div
+        className="xui-container"
+        style={{
+          overflow: "hidden",
+          border: "1px solid red",
+        }}
+      >
+        {/* 184450555 */}
         <h1>Playground</h1>
 
         <h4>See some of my free time designs below</h4>
 
-        <section
-          className="xui-d-flex xui-flex-dir-row xui-flex-wrap-nowrap xui-py-1  xui-overflow-hidden"
-          style={{
-            animation: "slideRight 60s linear infinite",
-            display: "flex",
-            gap: "0.5rem",
-          }}
-        >
-          {[1, 2].map((set) => (
-            <div key={set} style={{ display: "flex", gap: "0.5rem" }}>
-              <div className="xui-mx-1">
+        <Marquee pauseOnHover={true}>
+         
+            <div className="xui-d-grid  xui-grid-col-5 xui-py-1 xui-grid-gap-1 xui-overflow-hidden">
+              <div className="xui-mx-1 xui-w-fluid-100 xui-h-fluid-100 xui-bdr-w-2 xui-bdr-s-solid xui-bdr-blue ">
                 <img
                   src="/images/1.png"
                   alt="Image 1"
                   className="xui-object-fit-contain  xui-h-fluid-100 xui-w-fluid-100"
                 />
               </div>
-              <div className="xui-mx-1">
+              {/*  */}
+              <div className="xui-mx-1  xui-bdr-w-2 xui-bdr-s-solid xui-bdr-blue ">
                 <img
                   src="/images/2.png"
                   alt="Image 1"
                   className="xui-object-fit-contain  xui-h-fluid-100 xui-w-fluid-100"
                 />
               </div>
-              <div className="xui-mx-1">
+              <div className="xui-mx-1  xui-bdr-w-2 xui-bdr-s-solid xui-bdr-blue">
                 <img
                   src="/images/4.png"
                   alt="Image 1"
                   className="xui-object-fit-contain  xui-h-fluid-100 xui-w-fluid-100"
                 />
               </div>
-              <div className="xui-mx-1">
+              <div className="xui-mx-1  xui-bdr-w-2 xui-bdr-s-solid xui-bdr-blue ">
                 <img
                   src="/images/3.png"
                   alt="Image 1"
                   className="xui-object-fit-contain  xui-h-fluid-100 xui-w-fluid-100"
                 />
               </div>
-              <div className="xui-mx-1">
+              <div className="xui-mx-1  xui-bdr-w-2 xui-bdr-s-solid xui-bdr-blue">
                 <img
                   src="/images/2.png"
                   alt="Image 1"
                   className="xui-object-fit-contain  xui-h-fluid-100 xui-w-fluid-100"
                 />
               </div>
-              <div className="xui-mx-1">
-                <img
-                  src="/images/1.png"
-                  alt="Image 1"
-                  className="xui-object-fit-contain  xui-h-fluid-100 xui-w-fluid-100"
-                />
-              </div>
             </div>
-          ))}
-        </section>
+        
+        </Marquee>
         <style>{`
           @keyframes slideRight {
             0% {
